96e4b0b96b079e43a50dc8260ea4996f485bc6ab
Ksenia Developments <kseniadev@proton.me>
2023-08-25 04:20:25 +0600

    Kotlin version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.8.21"
-  kotlin("plugin.serialization") version "1.8.21"
+  kotlin("jvm") version "1.9.10"
+  kotlin("plugin.serialization") version "1.9.10"
   application
 }
 
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-  runtimeOnly("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.8.21")
+  runtimeOnly("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.9.10")
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   implementation("com.github.sisyphsu:dateparser:1.0.11")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.5.0")
@@ -23,7 +23,8 @@ dependencies {
   implementation("com.sksamuel.hoplite:hoplite-core:2.7.3")
   testImplementation("com.google.truth:truth:1.1.3")
   testImplementation("io.mockk:mockk:1.13.4")
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
 }
 
 application {
@@ -33,8 +34,14 @@ application {
 
 tasks.withType<KotlinCompile> {
   kotlinOptions {
-    freeCompilerArgs = listOf("-Xjsr305=strict", "-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
     jvmTarget = "19"
+    freeCompilerArgs += listOf(
+      "-Xjsr305=strict",
+      "-Xvalue-classes",
+      "-opt-in=kotlin.ExperimentalStdlibApi",
+      "-opt-in=kotlin.time.ExperimentalTime",
+      "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"
+    )
   }
 }
 
